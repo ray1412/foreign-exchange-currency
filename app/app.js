723585305ab6fcec0ae0211083cc97e9c75ea9ea
Raymond Haryanto <raymondharyanto.rh@gmail.com>
2018-11-17 21:08:00 +0700

    change anything related to favicon and html title

--- app/app.js
+++ app/app.js
@@ -32,6 +32,9 @@ import configureStore from './configureStore';
 // Import i18n messages
 import { translationMessages } from './i18n';
 
+// Semantic default css
+import 'semantic-ui-css/semantic.min.css';
+
 // Observe loading of Open Sans (to remove open sans, remove the <link> tag in
 // the index.html file and this observer)
 const openSansObserver = new FontFaceObserver('Open Sans', {});
