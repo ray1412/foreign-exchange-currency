62d5bcf1bfd5a17de87cf52c415fd6a8fb587975
Raymond Haryanto <raymondharyanto.rh@gmail.com>
2018-11-20 07:43:52 +0700

    fix import

--- app/containers/NotFoundPage/tests/index.test.js
+++ app/containers/NotFoundPage/tests/index.test.js
@@ -6,7 +6,6 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { FormattedMessage } from 'react-intl';
 
-import H1 from 'components/H1';
 import NotFound from '../index';
 
 describe('<NotFound />', () => {
@@ -14,12 +13,12 @@ describe('<NotFound />', () => {
     const renderedComponent = shallow(<NotFound />);
     expect(
       renderedComponent.contains(
-        <H1>
+        <React.Fragment>
           <FormattedMessage
             id="boilerplate.containers.NotFoundPage.header"
             defaultMessage="Page not found."
           />
-        </H1>,
+        </React.Fragment>,
       ),
     ).toEqual(true);
   });
